f54513fa73d6e324fe1283f0d29f344243825102
杨海帆 <2634481556@qq.com>
2022-08-29 22:04:00 +0800

    first commit

---
+++ examples/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <cs-button type="primary">按钮</cs-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+</style>
--- examples/main.js
+++ examples/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import CcUi from '../packages'
+
+Vue.use(CcUi)
 
 Vue.config.productionTip = false
 
